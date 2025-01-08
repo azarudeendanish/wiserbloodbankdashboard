2793ed72dd1706602cbdd3cb28ef2d5f7854cbbc
Azarudeen <azarudeendanish@gmail.com>
2025-01-09 01:31:38 +0530

    update async await

--- app/page.js
+++ app/page.js
@@ -22,53 +22,53 @@ export default function Home() {
 
   const getUserData = async () => {
     try {
-        const res = axios.get('/api/donors');
-        const data = res?.data;
-        console.log('donor user api data=>  ', res);
-        console.log('donor user api data=>  ', data);
-        if (data) {
-            setApiData(data);
-            setDonorCount(data.length);
-            console.log(data.length);
-            setBloodAPositive(data.filter((item)=> item.bloodgroup == 'A+').length)
-            setBloodANegative(data.filter((item)=> item.bloodgroup == 'A-').length)
-            setBloodBPositive(data.filter((item)=> item.bloodgroup == 'B+').length)
-            setBloodBNegative(data.filter((item)=> item.bloodgroup == 'B-').length)
-            setBloodABPositive(data.filter((item)=> item.bloodgroup == 'AB+').length)
-            setBloodABNegative(data.filter((item)=> item.bloodgroup == 'AB-').length)
-            setBloodOPositive(data.filter((item)=> item.bloodgroup == 'O+').length)
-            setBloodONegative(data.filter((item)=> item.bloodgroup == 'O-').length)
-            // let filterData = data.filter((item)=> item.bloodgroup == 'A+');
-            // console.log(filterData.length);
-          
-            
-            
-        }
-    } catch (error) {
-        console.log('api error');
-    }
-}
+      const res = await axios.get('/api/donors');
+      const data = await res?.data;
+      console.log('donor user api data=>  ', res);
+      console.log('donor user api data=>  ', data);
+      if (data) {
+        setApiData(data);
+        setDonorCount(data.length);
+        console.log(data.length);
+        setBloodAPositive(data.filter((item) => item.bloodgroup == 'A+').length)
+        setBloodANegative(data.filter((item) => item.bloodgroup == 'A-').length)
+        setBloodBPositive(data.filter((item) => item.bloodgroup == 'B+').length)
+        setBloodBNegative(data.filter((item) => item.bloodgroup == 'B-').length)
+        setBloodABPositive(data.filter((item) => item.bloodgroup == 'AB+').length)
+        setBloodABNegative(data.filter((item) => item.bloodgroup == 'AB-').length)
+        setBloodOPositive(data.filter((item) => item.bloodgroup == 'O+').length)
+        setBloodONegative(data.filter((item) => item.bloodgroup == 'O-').length)
+        // let filterData = data.filter((item)=> item.bloodgroup == 'A+');
+        // console.log(filterData.length);
 
-useEffect(() => {
+
+
+      }
+    } catch (error) {
+      console.log('api error');
+    }
+  }
+
+  useEffect(() => {
     getUserData();
-}, [donorCount]);
+  }, []);
   return (
     <>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {donorCount ? <Card title="Donors Count" value={donorCount} type="customers" />: <CardSkeleton /> }
+        {donorCount ? <Card title="Donors Count" value={donorCount} type="customers" /> : <CardSkeleton />}
       </div>
       <div className="mt-5 mb-2">Blood Stock</div>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {bloodAPositive ?  <Card title="A+" value={bloodAPositive} type="blood" /> : ''}
+        {bloodAPositive ? <Card title="A+" value={bloodAPositive} type="blood" /> : ''}
         {bloodANegative ? <Card title="A-" value={bloodANegative} type="blood" /> : ''}
-        {bloodBPositive ? <Card title="B+" value={bloodBPositive} type="blood" />: ''}
-        {bloodBNegative ? <Card title="B-" value={bloodBNegative} type="blood" />: ''}
+        {bloodBPositive ? <Card title="B+" value={bloodBPositive} type="blood" /> : ''}
+        {bloodBNegative ? <Card title="B-" value={bloodBNegative} type="blood" /> : ''}
         {bloodABPositive ? <Card title="AB+" value={bloodABPositive} type="blood" /> : ''}
         {bloodABNegative ? <Card title="AB-" value={bloodABNegative} type="blood" /> : ''}
         {bloodOPositive ? <Card title="O+" value={bloodOPositive} type="blood" /> : ''}
         {bloodONegative ? <Card title="O-" value={bloodONegative} type="blood" /> : ''}
-        </div>
-         {/* <Card title="Total Invoices" value={numberOfInvoices} type="blood" />
+      </div>
+      {/* <Card title="Total Invoices" value={numberOfInvoices} type="blood" />
         <Card
           title="Blood unit available"
           value={donorCount}
